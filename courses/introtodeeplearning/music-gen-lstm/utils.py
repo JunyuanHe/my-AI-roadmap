0606cd1360f88a4fd3786cbc7759eba06a668a00
Junyuan He <hejunyuan01@126.com>
2026-06-03 18:54:30 +0800

    Add utilities for music generation and plotting
    - Implemented functions to save songs in ABC format, convert ABC to WAV, and play WAV files.
    - Added a PeriodicPlotter class for real-time plotting with customizable axes and scaling options.
    - Created a bash script for converting ABC files to WAV using abc2midi and timidity.
    - Introduced a new checkpoint file for training data.

--- courses/introtodeeplearning/music-gen-lstm/utils.py
+++ courses/introtodeeplearning/music-gen-lstm/utils.py
@@ -1,6 +1,12 @@
 import os
 import regex as re
 
+from IPython.display import Audio
+
+
+
+cwd = os.path.dirname(__file__)
+
 def load_training_data():
     with open(os.path.join(cwd, "data", "irish.abc"), "r") as f:
         text = f.read()
@@ -14,3 +20,24 @@ def extract_song_snippet(text):
     songs = [song[1] for song in search_results]
     print("Found {} songs in text".format(len(songs)))
     return songs
+
+def save_song_to_abc(song, filename="tmp"):
+    save_name = "{}.abc".format(filename)
+    with open(save_name, "w") as f:
+        f.write(song)
+    return filename
+
+def abc2wav(abc_file):
+    path_to_tool = os.path.join(cwd, "bin", "abc2wav")
+    cmd = "{} {}".format(path_to_tool, abc_file)
+    return os.system(cmd)
+
+def play_wav(wav_file):
+    return Audio(wav_file)
+
+def play_song(song):
+    basename = save_song_to_abc(song)
+    ret = abc2wav(basename + ".abc")
+    if ret == 0:  # did not suceed
+        return play_wav(basename + ".wav")
+    return None
